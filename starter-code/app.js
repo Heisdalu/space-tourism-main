234ff616f4de6610375ffceb521f631804078af8
doubara <ayebadoubara@gmail.com>
2022-02-06 13:40:20 -0800

    upgrade

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,38 +1,33 @@
-(function(){
-    const toggle = document.querySelector("#mb-toggle")
-    const ul = document.querySelector("#ul--mobile")
+(function (){
+    const toggle = document.querySelector("#mb-toggle");
+    const ul = document.querySelector("#ul--mobile");
 
     const mobileToggle = () =>{
         toggle.addEventListener("click", ()=>{
             
-            ul.classList.toggle("show-link")
-        })
+            ul.classList.toggle("show-link");
+        });
         
         
-    }
-    mobileToggle()
+    };
+    mobileToggle();
     let destLinks = document.querySelectorAll(".main .dest-click")
-<<<<<<< HEAD
     const destLinksTwo = document.querySelectorAll(".dot")
     const destLinksTech = document.querySelectorAll(".circle-link .circle button")
-   const switchPlanets = (buttons) =>{
-=======
-   const switchPlanets = () =>{
->>>>>>> b9173f168bf8f7df1567a03a3f533238029a9b88
-       const sectionList = document.querySelectorAll(".main")
-       buttons.forEach(element => {
-           element.addEventListener("click", (e)=>{
-               sectionList.forEach(section=>{
-                   if (element.dataset.id === section.id){
-                       section.classList.add("show-main")
-                   }
-                   else section.classList.remove("show-main")
-               })
-           })
-       });
-   }
+    const switchPlanets = (buttons) =>{
+        const sectionList = document.querySelectorAll(".main")
+        buttons.forEach(element => {
+            element.addEventListener("click", (e)=>{
+                sectionList.forEach(section=>{
+                    if (element.dataset.id === section.id){
+                        section.classList.add("show-main")
+                    }
+                    else section.classList.remove("show-main")
+                })
+            })
+        })
+    }
    switchPlanets(destLinks)
    switchPlanets(destLinksTwo)
    switchPlanets(destLinksTech)
-
 })()
